a8fccd05dbf3fec1d711040400845796051a7066
Lari Hotari <lari.hotari@gradleware.com>
2015-04-13 09:14:51 -0400

    test that watches get registered for both type of input

--- subprojects/core/src/test/groovy/org/gradle/internal/filewatch/jdk7/FileWatcherExecutorTest.groovy
+++ subprojects/core/src/test/groovy/org/gradle/internal/filewatch/jdk7/FileWatcherExecutorTest.groovy
@@ -16,13 +16,18 @@
 
 package org.gradle.internal.filewatch.jdk7
 
+import org.gradle.api.file.DirectoryTree
+import org.gradle.api.internal.file.collections.DirectoryFileTree
 import org.gradle.internal.filewatch.FileWatchListener
 import org.gradle.internal.filewatch.FileWatcher
 import spock.lang.Specification
 
+import java.nio.file.FileSystem
 import java.nio.file.Path
 import java.nio.file.WatchKey
 import java.nio.file.WatchService
+import java.nio.file.attribute.BasicFileAttributes
+import java.nio.file.spi.FileSystemProvider
 import java.util.concurrent.atomic.AtomicBoolean
 
 /**
@@ -36,6 +41,7 @@ class FileWatcherExecutorTest extends Specification {
     def files
     def watchService
     def fileWatcherExecutor
+    int maxWatchLoops = 0
     Map<File, Path> dirToPathMocks = [:]
 
     def setup() {
@@ -46,6 +52,8 @@ class FileWatcherExecutorTest extends Specification {
         files = []
         watchService = Mock(WatchService)
         fileWatcherExecutor = new FileWatcherExecutor(fileWatcher, runningFlag, fileWatchListener, directories, files) {
+            int watchLoopCounter = 0
+
             @Override
             protected boolean supportsWatchingSubTree() {
                 return false
@@ -65,22 +73,49 @@ class FileWatcherExecutorTest extends Specification {
                 }
                 p
             }
+
+            @Override
+            protected boolean watchLoopRunning() {
+                watchLoopCounter++ < maxWatchLoops
+            }
         }
     }
 
     def "test registering inputs"() {
         given:
-        runningFlag.set(false)
         def file = new File("a/b/c")
         files << file
-        def pathMock = Mock(Path)
-        dirToPathMocks.put(file.getParentFile(), pathMock)
-        def watchKey = Mock(WatchKey)
+        def filePathMock = Mock(Path)
+        dirToPathMocks.put(file.getParentFile(), filePathMock)
+        def fileWatchKey = Mock(WatchKey)
+
+        def dir = new File("a2/b2")
+        def directoryTree = Mock(DirectoryTree)
+        directoryTree.getDir() >> {
+            dir
+        }
+        directories << directoryTree
+        def dirPathMock = Mock(Path)
+        def fileSystemProvider = Mock(FileSystemProvider)
+        def fileSystem = Mock(FileSystem)
+        fileSystem.provider() >> fileSystemProvider
+        dirPathMock.getFileSystem() >> fileSystem
+        dirToPathMocks.put(dir, dirPathMock)
+        def dirWatchKey = Mock(WatchKey)
+        def dirAttributes  = Mock(BasicFileAttributes)
+
         when:
         fileWatcherExecutor.run()
         then:
-        pathMock.register(watchService, FileWatcherExecutor.WATCH_KINDS, FileWatcherExecutor.WATCH_MODIFIERS) >> {
-            watchKey
+        filePathMock.register(watchService, FileWatcherExecutor.WATCH_KINDS, FileWatcherExecutor.WATCH_MODIFIERS) >> {
+            fileWatchKey
         }
+        dirPathMock.register(watchService, FileWatcherExecutor.WATCH_KINDS, FileWatcherExecutor.WATCH_MODIFIERS) >> {
+            dirWatchKey
+        }
+        fileSystemProvider.readAttributes(dirPathMock, _, _) >> {
+            dirAttributes
+        }
+        watchService.close()
     }
 }
